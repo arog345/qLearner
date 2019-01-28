f498eafce2d9b686e53da80428b1be7a75f4d0d8
jpkatz <46250338+jpkatz@users.noreply.github.com>
2019-01-27 18:37:32 -0600

    Update functions.py
    Added plotting as a function to keep mainScript clean

--- functions.py
+++ functions.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import networkx as nx
+from matplotlib import pyplot
+
 def buildGraph(numberNodes):
     am = np.array([[1 if np.random.rand(1,1)>0.7 and x>y else 0 for x in range(numberNodes)]
     for y in range(numberNodes)]) #adjacent Matrix
@@ -15,3 +18,32 @@ def buildGraph(numberNodes):
     amT = np.transpose(am) #transpose the matrix
     amF = am + amT #Full adjacency matrix
     return amF
+
+def plotGraph(adjacencyMatrix,numberNodes):
+    G = nx.from_numpy_matrix(adjacencyMatrix)
+    
+    # Build the two plots
+    pyplot.figure()
+    
+    # Plot for the adjacency matrix
+    pyplot.subplot(1, 2, 1)
+    pyplot.imshow(
+        adjacencyMatrix,
+        cmap="Greys",
+        interpolation="none",
+        extent=[0, numberNodes, 0, numberNodes],
+    )
+    ax1 = pyplot.gca()
+    
+    # Centers the labels on the axes
+    ax1.xaxis.tick_top()
+    ax1.set_xticks(np.arange(0.5, numberNodes + 0.5, 1))
+    ax1.set_yticks(np.arange(0.5, numberNodes + 0.5, 1))
+    ax1.set_xticklabels(np.arange(0, numberNodes, 1))
+    ax1.set_yticklabels(np.arange(numberNodes - 1, -1, -1))
+    
+    # Plot of the graph
+    pyplot.subplot(1, 2, 2)
+    nx.draw(G, with_labels=True, font_weight="bold")
+    
+    pyplot.show()
